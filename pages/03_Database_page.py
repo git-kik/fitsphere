eff844597f4267bb4c32bceeeff4fb27a90725aa
aikik <105649137+git-kik@users.noreply.github.com>
2024-04-23 08:00:21 +0545

    Update 03_Database_page.py

--- pages/03_Database_page.py
+++ pages/03_Database_page.py
@@ -55,22 +55,50 @@ def delete_record(image_id):
     except Exception as e:
         st.error(f"An error occurred while deleting the record: {e}")
 
+# Function to search for an image by image filename
+def search_by_image_filename(image_filename):
+    try:
+        # Query the database for information about the image with the given filename
+        c.execute("SELECT id, image_path, prediction, confidence FROM predictions WHERE image_path LIKE ?", ('%' + image_filename + '%',))
+        results = c.fetchall()
+
+        # Display the information about the image
+        if results:
+            for result in results:
+                image_id, image_path, prediction, confidence = result
+                st.write(f"Image ID: {image_id}")
+                st.image(image_path, caption='Uploaded Image', use_column_width=True)
+                st.write(f"Prediction: {prediction}")
+                st.write(f"Confidence: {confidence}")
+        else:
+            st.write("Image not found.")
+    except Exception as e:
+        st.error(f"An error occurred while searching for the image: {e}")
+
 
 # Main function
 def main():
     st.title('Database of Saved Images with Predictions')
 
+    # Search function
+    st.sidebar.subheader("Search Image")
+    image_filename = st.sidebar.text_input("Enter Image Path")
+    if st.sidebar.button("Search"):
+        search_by_image_filename(image_filename)
+
     # Sidebar with index
     st.sidebar.title('Index')
-    prediction_index = st.sidebar.radio('Select Prediction', ['Not suffering from Pneumonia', 'Suffering from Bacterial Pneumonia', 'Suffering from Viral Pneumonia'])
+    prediction_index = st.sidebar.radio('Select Prediction', ['Not suffering from Pneumonia (0)', 'Suffering from Bacterial Pneumonia (1)', 'Suffering from Viral Pneumonia (2)'])
 
     # Displaying saved images and their predictions based on index selection
-    if prediction_index == 'Not suffering from Pneumonia':
+    if prediction_index == 'Not suffering from Pneumonia (0)':
         display_saved_images_with_predictions_for_index(0)
-    elif prediction_index == 'Suffering from Bacterial Pneumonia':
+    elif prediction_index == 'Suffering from Bacterial Pneumonia (1)':
         display_saved_images_with_predictions_for_index(1)
-    elif prediction_index == 'Suffering from Viral Pneumonia':
+    elif prediction_index == 'Suffering from Viral Pneumonia (2)':
         display_saved_images_with_predictions_for_index(2)
+
+    
 
 def display_saved_images_with_predictions_for_index(prediction):
     # Query the database to retrieve the paths and predictions of saved images
